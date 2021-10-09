d1b2d740c817e2b4a92baefc8ec807dbf60aeca3
Christof Konstantinopoulos <KvardekDu@ckondo.de>
2021-10-09 22:00:59 +0200

    wrong compare operator in input check

--- ex00/ft_check_input.c
+++ ex00/ft_check_input.c
@@ -6,7 +6,7 @@
 /*   By: ckonstan <KvardekDu@ckondo.de>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 17:50:29 by ckonstan          #+#    #+#             */
-/*   Updated: 2021/10/09 21:49:46 by ckonstan         ###   ########.fr       */
+/*   Updated: 2021/10/09 21:58:32 by ckonstan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	ft_check_input(int argc, char *args[])
 		return (-1);
 	parameter = args[1];
 	i = 0;
-	if (parameter[i] < '1' && parameter[i] > '4')
+	if (parameter[i] < '1' || parameter[i] > '4')
 		return (-2);
 	i++;
-	while (parameter[i] != 0)
+	while (parameter[i] != 0 || i > 31)
 	{
 		if (parameter[i] == ' ')
 			i++;
-		if (parameter[i] < '1' && parameter[i] > '4')
+		if (parameter[i] < '1' || parameter[i] > '4')
 			return (-2);
 		i++;
 	}
