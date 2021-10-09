b34275b2261509d3adc1fe6d90296021765cb831
Christof Konstantinopoulos <KvardekDu@ckondo.de>
2021-10-09 23:52:49 +0200

    test script edited, comments added to test output

--- ex00/rush01-test.sh
+++ ex00/rush01-test.sh
@@ -1,25 +1,34 @@
 #! /bin/bash
 
+clear
 gcc -Wall -Wextra -Werror -o rush-01 *.c
 norminette -R CheckForbiddenSourceHeader
 echo "--- Tests start ---"
 
 test="4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2"
-echo input: '>'"$test"'<'
+echo input: '>'"$test"'<' OK
 ./rush-01 "$test" | cat -e
 
 test=""
-echo input: '>'"$test"'<'
+echo input: '>'"$test"'<' empty parameter
 ./rush-01 "$test" | cat -e
 
 test=" 4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2"
-echo input: '>'"$test"'<'
+echo input: '>'"$test"'<' leading space
+./rush-01 "$test" | cat -e
+
+test="4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2 "
+echo input: '>'"$test"'<' following space
 ./rush-01 "$test" | cat -e
 
 test="4 3 2 1 1 2 2 2 4 3 2 1 1 2 2-2"
-echo input: '>'"$test"'<'
+echo input: '>'"$test"'<' minus instead of last space
 ./rush-01 "$test" | cat -e
 
 test="4 3 5 1 1 2 2 2 4 3 2 1 1 2 2 2"
-echo input: '>'"$test"'<'
+echo input: '>'"$test"'<' third digit 5 to big
+./rush-01 "$test" | cat -e
+
+test="4 3 2 1 1 2 2 2 4 3 2 1 1 2 2"
+echo input: '>'"$test"'<' one digit missing
 ./rush-01 "$test" | cat -e
